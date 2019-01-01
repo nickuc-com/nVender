3957e7189a754bd87ff570e217e0faa70f72f336
NickUC <nickultracraft@gmail.com>
2019-01-01 20:41:44 -0200

    Version 1.1

---
+++ src/me/NickUltracraft/Vender/Core/Gui.java
@@ -0,0 +1,85 @@
+package me.NickUltracraft.Vender.Core;
+
+import java.util.ArrayList;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class Gui {
+	
+	private Player p;
+	public static ArrayList<String> vendaShift = new ArrayList<>();
+	public static ArrayList<String> autoVenda = new ArrayList<>();
+	
+	private enum VendaType {
+		AUTO_VENDA, VENDA_SHIFT, VENDA;
+	}
+	private enum VendaStatus {
+		ATIVADO, DESATIVADO, NEUTRO;
+	}
+	public Gui(Player p) {
+		this.p = p;
+		open();
+	}
+	private VendaStatus getVendaType(VendaType vendaType) {
+		switch (vendaType) {
+		case AUTO_VENDA:
+			if(autoVenda.contains(p.getName())) {
+				return VendaStatus.ATIVADO;
+			} else {
+				return VendaStatus.DESATIVADO;
+			}
+		case VENDA_SHIFT:
+			if(vendaShift.contains(p.getName())) {
+				return VendaStatus.ATIVADO;
+			} else {
+				return VendaStatus.DESATIVADO;
+			}
+		case VENDA:
+			return VendaStatus.NEUTRO;
+		}
+		return VendaStatus.DESATIVADO;
+	}
+	private String getDisplayName(VendaType status) {
+		switch (status) {
+		case AUTO_VENDA:
+			return "§7Auto-Venda";
+		case VENDA:
+			return "§7Vender";
+		case VENDA_SHIFT:
+			return "§7Venda Shift";
+		}
+		return "";
+	}
+	private ItemStack getItemStack(VendaType vendaType, VendaStatus vendaStatus) {
+		ItemStack item;
+		if(vendaType == VendaType.AUTO_VENDA || (vendaType == VendaType.VENDA_SHIFT)) {
+			item = new ItemStack(Material.WOOL);
+		} else {
+			item = new ItemStack(Material.NAME_TAG);
+		}
+		ItemMeta itemMeta = item.getItemMeta();
+		itemMeta.setDisplayName(getDisplayName(vendaType));
+		if(vendaStatus == VendaStatus.DESATIVADO) {
+			item.setDurability((short)14);
+		} else if(vendaStatus == VendaStatus.ATIVADO) {
+			item.setDurability((short)5);
+		} 
+		item.setItemMeta(itemMeta);
+		itemMeta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+		return item;
+	}
+	private void open() {
+		Inventory inv = Bukkit.createInventory(null, 27, "§8Opções de Venda");
+		inv.setItem(11, getItemStack(VendaType.AUTO_VENDA, getVendaType(VendaType.AUTO_VENDA)));
+		inv.setItem(13, getItemStack(VendaType.VENDA, getVendaType(VendaType.VENDA)));
+		inv.setItem(15, getItemStack(VendaType.VENDA_SHIFT, getVendaType(VendaType.VENDA_SHIFT)));
+		p.openInventory(inv);
+	}
+
+}
---
+++ src/me/NickUltracraft/Vender/Core/VendaListener.java
@@ -0,0 +1,92 @@
+package me.NickUltracraft.Vender.Core;
+
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import me.NickUltracraft.Vender.Main;
+
+public class VendaListener implements Listener {
+	
+	private static ArrayList<String> delay = new ArrayList<>();
+	
+	@EventHandler
+	public void onClick(InventoryClickEvent e) {
+		if(e.getWhoClicked() != null && (e.getInventory() != null)) {
+			Player p = (Player)e.getWhoClicked();
+			if(e.getInventory().getName().equalsIgnoreCase("§8Opções de Venda")) {
+				e.setCancelled(true);
+				ItemStack item = e.getCurrentItem();
+				if(item != null && (item.hasItemMeta() && (item.getItemMeta().hasDisplayName()))) {
+					String display = item.getItemMeta().getDisplayName();
+					switch (display) {
+					case "§7Auto-Venda":
+						if(Gui.autoVenda.contains(p.getName())) {
+							p.sendMessage("§cVocê desativou o modo de venda automática.");
+							Gui.autoVenda.remove(p.getName());
+							p.closeInventory();
+							new Gui(p);
+						} else {
+							p.sendMessage("§aVocê ativou o modo de venda automática.");
+							Gui.autoVenda.add(p.getName());
+							Timer timer = new Timer(true);
+							timer.scheduleAtFixedRate(new TimerTask() {
+								@Override
+								public void run() {
+									if(Gui.autoVenda.contains(p.getName())) {
+										new Vender(p);
+									} else {
+										cancel();
+									}
+								}
+							}, 1000*5, 1000*5);
+							p.closeInventory();
+							new Gui(p);
+						}
+						return;
+					case "§7Vender":
+						new Vender(p);
+						return;
+					case "§7Venda Shift":
+						if(Gui.vendaShift.contains(p.getName())) {
+							p.sendMessage("§cVocê desativou o modo de venda por shift.");
+							Gui.vendaShift.remove(p.getName());
+							p.closeInventory();
+							new Gui(p);
+						} else {
+							p.sendMessage("§aVocê ativou o modo de venda por shift.");
+							Gui.vendaShift.add(p.getName());
+							p.closeInventory();
+							new Gui(p);
+						}
+						return;
+					}
+				}
+			}
+		}
+	}
+	@EventHandler
+	public void onShift(PlayerToggleSneakEvent e) {
+		if(Gui.vendaShift.contains(e.getPlayer().getName())) {
+			if(!delay.contains(e.getPlayer().getName())) {
+				new Vender(e.getPlayer());
+				delay.add(e.getPlayer().getName());
+				new BukkitRunnable() {
+					@Override
+					public void run() {
+						delay.remove(e.getPlayer().getName());
+						cancel();
+					}
+				}.runTaskLater(Main.m, 20);
+			} 
+		}
+	}
+}
